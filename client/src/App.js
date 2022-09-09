e8e73effffcb14b21992dfb08f0589bf956ad8bd
Dhinakaran001 <karandhina708@gmail.com>
2022-09-09 12:57:59 +0530

    added loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Main from "./component/Main";
 import DataProvider from "./GlobalContext";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./style/Theme";
+import BeatLoader from "react-spinners/BeatLoader";
 
-function App() {
+function App(props) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
-    <DataProvider>
-      <ThemeProvider theme={theme}>
-        <Main />
-      </ThemeProvider>
-    </DataProvider>
+    <div>
+      {loading ? (
+        <BeatLoader
+          size={20}
+          color={"#8956ff"}
+          loading={loading}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        />
+      ) : (
+        <DataProvider>
+          <ThemeProvider theme={theme}>
+            <Main />
+          </ThemeProvider>
+        </DataProvider>
+      )}
+    </div>
   );
 }
 
